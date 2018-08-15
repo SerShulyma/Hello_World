da96dc02f86bde02dedfa7aeb75273b94347db06
Сергей Шулыма <C:\Users\\AppData\Roaming\The Bat!\Mail>
2018-08-15 22:04:24 +0300

    add little loop

--- Lesson2/Source.cpp
+++ Lesson2/Source.cpp
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main(void)
 {
-	printf("Hello World");
-	printf("Hello Word!!!\n");
+	for (int i = 0; i <= 1; i++)
+	{
+		printf("Hello World");
+		printf("Hello Word!!!\n");
+	}
 	return 0;
 }
